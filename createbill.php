c0ecc4642edbbac5d690fe5c4052b72fcbfba6ca
charan7105 <saicharanvenna123@gmail.com>
2023-07-16 19:32:18 +0530

    spaces left

--- createbill.php
+++ createbill.php
@@ -22,7 +22,6 @@
       <img loading="lazy" src="pic.jpg" alt="..." width="80" height="80" class="mr-3 rounded-circle img-thumbnail shadow-sm">
       <div class="media-body">
         <h4 class="m-0"><?php echo $_SESSION['name']; ?></h4>
-       
       </div>
     </div>
   </div>
@@ -129,6 +128,7 @@
           $val6="";
         }
       ?>
+
   <!-- Demo content -->
   <h2 class="display-4 text-white text-center title">CREATE BILL</h2>
   <div class="separator"></div>
@@ -191,7 +191,6 @@
         </div>
       </div>
       </div>
-   
     
 </div>
 <hr style="border-top: 1px dashed rgb(255, 255, 255);">
@@ -337,27 +336,13 @@
       </div>
     </div>
 </div>
-  
-  
-  
-  <div class="row text-white">
-    
-</div>
+
 <div class="footer">
     <p>&copy; Copyright Bill Book</p>
 </div>  
-
-
-
-
 </div>
 
-
-
-
-
 <!-- End demo content -->
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
